c7b82a49499b44659d875d6bf9702dd65667d68f
Ansar Ahmad <ahmadansar264@gmail.com>
2025-06-12 15:56:06 +0500

    Included Express and created multiple routes using Express

--- Index.js
+++ Index.js
@@ -1,1 +1,26 @@
 console.log("Hello World!");
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (reg, res) => {
+    res.send("Hello World!")
+})
+
+app.get('/AboutUs', (reg, res) => {
+    res.send("<p>Hello My name is  Ansar Ahmad and Im a Software Engineer im an ASP.Net Core MVC Web Developer and currestly im learning MERN Stack Backend</p>")
+})
+
+app.get('/ContactUs', (req, res) => {
+    res.send("<h4>Gmail: ahmadansar264@gmail.com</h4>")
+})
+
+app.get('/Services', (req, res) => {
+    res.send("<ul><li>Service 1</li><li>Service 2</li><li>Service 3</li></ul>");
+});
+
+
+app.listen(port, () =>{
+    console.log('App listen on Port:' + port)
+})
